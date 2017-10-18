e5680c2c7d716b9df3e042055c4dc8b9145da8c8
Your Name <you@example.com>
2017-10-18 19:30:10 +0100

    1st commit

---
+++ lib/mykml.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+# file: mykml.rb
+
+require 'dynarex'
+require 'nokogiri'
+
+
+class MyKML
+
+  def initialize(logfile)
+
+    # parse the log file entries into a Dynarex file
+
+    @dx = Dynarex.new 'log/entry(time, lat, lon)'
+    @dx.format_mask = '\[[!time] #\d+\] *INFO -- : [!lat], [!lon]'
+
+    a = File.read(logfile).lines
+    a.shift if a.first =~ /^#/
+
+    @dx.import a.join
+
+  end
+
+  def to_kml()
+
+    doc   = Nokogiri::XML(@dx.to_xml(pretty: true))
+
+    xslfile = File.read(File.join(File.dirnam(__FILE__), '..', 
+                          'xslt','mykml.xsl')
+    xslt  = Nokogiri::XSLT(xslfile)
+    xslt.transform(doc)
+  end
+
+end
---
+++ mykml.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'mykml'
+  s.version = '0.1.0'
+  s.summary = 'Generates KML from a log file.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/mykml.rb', 'xsl/mykml.xsl']
+  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.26')
+  s.add_runtime_dependency('nokogiri', '~> 1.8', '>=1.8.1') 
+  s.signing_key = '../privatekeys/mykml.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/mykml'
+end
